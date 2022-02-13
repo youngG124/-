eca4159da1a2f4f3e9133aca44b2778a301be299
youngG124 <newyoung124@naver.com>
2022-02-13 22:19:26 +0900

    type of arr is object

--- server/test.js
+++ server/test.js
@@ -1,1 +1,7 @@
-console.log('hello node.js!');
+const arr = ['apple', 'banana', 'orange'];
+
+for (let i = 0; i < arr.length; i++ ) {
+    console.log(arr[i]);
+}
+
+console.log('type of arr is ' + typeof arr);
